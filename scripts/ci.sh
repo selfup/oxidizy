a642f34e7bda71a97393fe8bfa69f488cea29093
selfup <boudinot.regis@yahoo.com>
2022-03-03 20:02:00 -0600

    update workflow and ci script

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,28 +1,17 @@
 #!/usr/bin/env bash
 
-set -eo pipefail
+set -eou pipefail
 
 date
 
-status=$((git status -s | grep -q -e '.rs' -e 'Cargo.') && echo 1 || echo 0)
+rustup component add rustfmt
 
-if [[ $status != 0 ]]
-then
-    echo 'no need to run rust CI scripts..'
-    echo 'exiting succesfully..'
-    exit 0
-else
-    rustup component add rustfmt
+echo 'running rustfmt'
 
-    echo 'running rustfmt'
+cargo fmt --all -- --check
 
-    cargo fmt --all -- --check
+echo 'success!'
 
-    echo 'success!'
+echo 'running tests'
 
-    echo 'running tests'
-
-    ./scripts/test.sh
-
-    exit 0
-fi
+./scripts/test.sh
