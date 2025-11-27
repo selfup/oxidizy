b6f9b96249ae1be7b2f3d9db2a6bb59d63655778
Regis Boudinot <selfup@users.noreply.github.com>
2025-11-26 19:28:36 -0600

    adding sudo awareness to os script (#79)

--- scripts/os.sh
+++ scripts/os.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
 
-sudo apt-get update \
-    && sudo apt-get install g++ \
-        pkg-config libx11-dev \
-        libasound2-dev libudev-dev \
-        libwayland-dev -y
+if command -v sudo &> /dev/null
+then
+    RUN_CMD="sudo"
+    echo "--- sudo found, using sudo for privileged commands ---"
+else
+    RUN_CMD=""
+    echo "--- sudo not found, running commands without sudo ---"
+fi
+
+echo "--- Attempting to update system package list... ---"
+
+$RUN_CMD apt-get update
+
+echo "--- Attempting to add deps for bevy to run in pipeline... ---"
+
+$RUN_CMD apt-get install g++ \
+    pkg-config libx11-dev \
+    libasound2-dev libudev-dev \
+    libwayland-dev -y
+
+echo "--- OS dep CI script finished ---"
